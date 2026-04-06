714864b46fc54c3ff490bae2fd61a4b244e5c9ec
koushik965 <koushikchandaka@gmail.com>
2026-04-06 14:38:18 +0530

    Delete ad-recommendation-system directory

--- ad-recommendation-system/backend/routes/interactionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { logInteraction, getUserInteractions } = require('../controllers/interactionController');
-const { protect } = require('../middleware/auth');
-
-router.post('/', protect, logInteraction);
-router.get('/', protect, getUserInteractions);
-
-module.exports = router;
