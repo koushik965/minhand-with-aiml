5ba6ad2a3fa5820ac906a777347f34043b46e18f
koushik965 <koushikchandaka@gmail.com>
2026-04-07 21:45:11 +0530

    Update api.js

--- minhand/frontend/src/services/api.js
+++ minhand/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+console.log("🌍 API BASE URL:", process.env.REACT_APP_API_URL);
 const api = axios.create({
-  console.log("🌍 API BASE URL:", process.env.REACT_APP_API_URL);
+  
   baseURL: process.env.REACT_APP_API_URL + "/api",
   headers: { 'Content-Type': 'application/json' },
   timeout: 15000,
